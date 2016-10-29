15773f010e7427a2768e208e4e841b66876157ea
kamile <km662@cam.ac.uk>
2016-10-29 23:25:53 +0100

    Added Autocomplete DirectionTo field and get updated current location

--- app/src/main/java/uk/ac/cam/km662/arm_hackathon_2016/StartDirections.java
+++ app/src/main/java/uk/ac/cam/km662/arm_hackathon_2016/StartDirections.java
@@ -1,30 +1,223 @@
 package uk.ac.cam.km662.arm_hackathon_2016;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class StartDirections extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.model.LatLng;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+
+public class StartDirections extends AppCompatActivity implements OnConnectionFailedListener {
+
+    private static final String TAG = StartDirections.class.getSimpleName();
     private Button startNavigation;
-    private EditText inputDestination;
+    private PlaceAutocompleteFragment inputDestination;
+    private TextView editLocation;
+    private ProgressBar progressBar;
+
+    private GoogleApiClient mGoogleApiClient;
+
+    private Boolean flag = false;
+
+    private LocationListener locationListener = null;
+    private LocationManager locationManager = null;
+    Location currentLocation;
+    LatLng destinationLocation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_directions);
 
-        inputDestination = (EditText) findViewById(R.id.directions_to);
+        inputDestination = (PlaceAutocompleteFragment) getFragmentManager().findFragmentById(R.id.directions_to);
         startNavigation = (Button) findViewById(R.id.startNavigation);
+        editLocation = (TextView) findViewById(R.id.locationText);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar1);
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
 
+        progressBar.setVisibility(View.INVISIBLE);
+        progressBar.getIndeterminateDrawable().setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, this)
+                .build();
+        mGoogleApiClient.connect();
+
+        inputDestination.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                destinationLocation = place.getLatLng();
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.getName());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+
+        startNavigation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flag = displayGpsStatus();
+                if (flag) {
+                    editLocation.setText("Move device to see changes.");
+                    progressBar.setVisibility(View.VISIBLE);
+
+                    locationListener = new MyLocationListener();
+                    try {
+                        locationManager.requestLocationUpdates(LocationManager
+                                .GPS_PROVIDER, 5000, 10, locationListener);
+                    } catch (SecurityException e) {
+                        System.out.print("No permission to access location.");
+                    }
+
+                } else {
+                    alertbox("Gps Status!!", "Your GPS is: OFF");
+                }
+            }
+        });
     }
 
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        //Could not connect to Google APIs.
+    }
 
+    protected void alertbox(String title, String mymessage) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Your Device's GPS is Disable")
+                .setCancelable(false)
+                .setTitle("** Gps Status **")
+                .setPositiveButton("Gps On",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // finish the current activity
+                                // AlertBoxAdvance.this.finish();
+                                Intent myIntent = new Intent(
+                                        Settings.ACTION_SECURITY_SETTINGS);
+                                startActivity(myIntent);
+                                dialog.cancel();
+                            }
+                        })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // cancel the dialog box
+                                dialog.cancel();
+                            }
+                        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private Boolean displayGpsStatus() {
+        ContentResolver contentResolver = getBaseContext()
+                .getContentResolver();
+        boolean gpsStatus = Settings.Secure
+                .isLocationProviderEnabled(contentResolver,
+                        LocationManager.GPS_PROVIDER);
+        if (gpsStatus) {
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+
+    private class MyLocationListener implements LocationListener {
+        @Override
+        public void onLocationChanged(Location loc) {
+
+//            editLocation.setText("");
+//            progressBar.setVisibility(View.INVISIBLE);
+//            Toast.makeText(getBaseContext(),"Location changed : Lat: " +
+//                            loc.getLatitude()+ " Lng: " + loc.getLongitude(),
+//                    Toast.LENGTH_SHORT).show();
+//            String longitude = "Longitude: " +loc.getLongitude();
+//            Log.v(TAG, longitude);
+//            String latitude = "Latitude: " +loc.getLatitude();
+//            Log.v(TAG, latitude);
+//
+//    /*----------to get City-Name from coordinates ------------- */
+//            String cityName=null;
+//            Geocoder gcd = new Geocoder(getBaseContext(),
+//                    Locale.getDefault());
+//            List<Address>  addresses;
+//            try {
+//                addresses = gcd.getFromLocation(loc.getLatitude(), loc
+//                        .getLongitude(), 1);
+//                if (addresses.size() > 0)
+//                    System.out.println(addresses.get(0).getLocality());
+//                cityName=addresses.get(0).getLocality();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//            String s = longitude+"\n"+latitude +
+//                    "\n\nMy Currrent City is: "+cityName;
+//            editLocation.setText(s);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onStatusChanged(String provider,
+                                    int status, Bundle extras) {
+            // TODO Auto-generated method stub
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
